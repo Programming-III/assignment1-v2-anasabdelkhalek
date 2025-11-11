6a5caf364e675f5d43641ebc5bce784bb916e5de
anasabdelkhalek <anas.abdelkhalek@student.giu-uni.de>
2025-11-11 09:57:26 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -5,17 +5,12 @@
 #include <string>
 #using namespace std;
 //#write student class here
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Student : public Person{
+    private:
+    int yearLevel;
+    string major;
+    public:
+    Student(string m,int y);
+    void DisplayS();
+};
 #endif
