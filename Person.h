e6b0d58fd7c0ea293e8b2b90247a9134319c5bbc
anasabdelkhalek <anas.abdelkhalek@student.giu-uni.de>
2025-11-11 09:56:40 +0200

    Implement Person class with constructor and display method
    Added Person class with private attributes and public methods.

--- Person.h
+++ Person.h
@@ -5,16 +5,13 @@
 using namespace std;
 
 //#write Person class here
+ class Person {
+   private:
+    string name;
+    int id;
 
-
-
-
-
-
-
-
-
-
-
-
+   public:
+   Person(string n,int i);
+   void Display();
+};
 #endif
