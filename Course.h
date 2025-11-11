aa3d88349f70a2e5076e1d1496ccbe1164f4c951
anasabdelkhalek <anas.abdelkhalek@student.giu-uni.de>
2025-11-11 09:55:12 +0200

    Implement Course class with student management
    Added Course class with attributes and methods.

--- Course.h
+++ Course.h
@@ -5,18 +5,17 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Course {
+    private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    public:
+    Course(string co,string cn,int mx,int cs);
+    addStudent(const Student& s);
+    void displayCourseInfo();
+};
 
 #endif
